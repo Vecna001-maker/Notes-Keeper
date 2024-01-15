9df82435b54d211c31b9fb0391a61cde08671ae3
Vecna001-maker <123670787+Vecna001-maker@users.noreply.github.com>
2024-01-15 13:07:11 +0530

    db atlas added

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
+const dbs="mongodb+srv://sanchit123:sanchit123@cluster0.mqttmqf.mongodb.net/?retryWrites=true&w=majority" 
+// | "mongodb://127.0.0.1:27017";
 const connectToMongo = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017', {
+    await mongoose.connect(dbs, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
