a2c399bce06d6912ec34c645a7c062ae9be813a5
Vecna001-maker <123670787+Vecna001-maker@users.noreply.github.com>
2024-01-15 13:02:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import {useState} from 'react'
+
+
+import Navbar from './components/Navbar';
+import NoteState from './context/notes/NoteState';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Alert from './components/Alert';
 
 function App() {
+
+  const [alert, setalert] = useState(null);
+
+  const showAlert = (type,message) =>{
+      setalert({type:type,message:message});
+      setTimeout(() => {
+        setalert(null);
+      }, 1500);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NoteState>
+
+
+        <Router>
+          <Navbar />
+
+           {alert !== null ? <Alert alert={alert} />: ""}
+
+          
+
+          <div className='container'>
+            <Routes>
+              <Route exact path='/' element={<Home setalert={setalert} alert={alert} showAlert={showAlert} />} />
+              <Route exact path='/about' element={<About setalert={setalert} alert={alert} showAlert={showAlert} />} />
+              <Route exact path='/login' element={<Login setalert={setalert} alert={alert} showAlert={showAlert}/>} />
+              <Route exact path='/signup' element={<Signup setalert={setalert} alert={alert} showAlert={showAlert}/>} />
+            </Routes>
+          </div>
+
+
+
+        </Router>
+      </NoteState>
+    </>
+
   );
 }
 
